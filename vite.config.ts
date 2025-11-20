6cd4c593442a4e29d165d9c58e457a5d5b66e294
theujwalthakre <thakare2829@gmail.com>
2025-11-20 23:58:48 +0530

    nm

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Use a root-relative base so assets work on custom domains and GitHub Pages
-    base: "/"
+    base: "/theujwalthakare.dev/"
 })
