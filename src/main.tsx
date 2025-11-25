7cee4648f01920f3da502704df673262be4aeeae
theujwalthakre <thakare2829@gmail.com>
2025-11-25 20:23:53 +0530

    vb

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import { HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <HashRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <App />
+    </HashRouter>
   </StrictMode>,
 );
